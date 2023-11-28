9077494589ec9fdcd8e59eda7db10b0685455528
miniial <109952039+miniial@users.noreply.github.com>
2023-11-28 19:23:24 +0530

    Add files via upload

---
+++ springboot-microservices/springboot-microservices/employee-service/src/main/java/net/javaguides/employeeservice/entity/Employee.java
@@ -0,0 +1,34 @@
+package net.javaguides.employeeservice.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String departmentCode;
+}
